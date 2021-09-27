4767d96461ccc5e186f5a3acc51c6133b43d8be3
Hugo Wangler <hugwan-6@student.ltu.se>
2021-09-27 13:06:41 +0200

    Solution exercise 3

--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -10,6 +10,10 @@
 
 package main
 
+import (
+	"time"
+)
+
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
 type User struct {
@@ -21,7 +25,25 @@ type User struct {
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
-	process()
+	start := time.Now()
+	if u.IsPremium {
+		process()
+		u.TimeUsed += int64(time.Since(start))
+	} else {
+		c := make(chan int)
+		go func() {
+			process()
+			c <- 1
+		}()
+
+		select {
+		case <-c:
+			u.TimeUsed += int64(time.Since(start))
+			return true
+		case <-time.After((time.Second * 10) - time.Duration(u.TimeUsed)):
+			return false
+		}
+	}
 	return true
 }
 
